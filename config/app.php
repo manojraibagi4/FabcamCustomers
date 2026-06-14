d39a40f46a24e8365be4a98758623767625d799c
Manoj Raibagi <manojraibagi4@gmail.com>
2026-06-14 13:44:47 +0530

    app php chnages

--- config/app.php
+++ config/app.php
@@ -1,13 +1,16 @@
 <?php
 
 define('APP_NAME', 'Fabcam Technologies');
-define('APP_URL',  'http://localhost:8000');
-define('APP_ENV',  'development');
+define('APP_ENV',  'production');
+
+$isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+        || (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
+define('APP_URL', ($isHttps ? 'https' : 'http') . '://' . ($_SERVER['HTTP_HOST'] ?? 'customers.fabcam.in'));
 
 // Set BASE_PATH if app lives in a subdirectory e.g. '/FabcamNewWebsite'
 // Leave empty string '' if served from doc root
 define('BASE_PATH', '');
-define('BASE_URL',  APP_URL . BASE_PATH);
+define('BASE_URL', APP_URL . BASE_PATH);
 
 if (APP_ENV === 'development') {
     error_reporting(E_ALL);
